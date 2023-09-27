c24b1d3a3cca6ece56512a331732dad32589a5e5
root <zach>
2023-09-27 19:12:51 +0000

    update

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# builds everything without installing
+#
+# Modal AI Inc. 2023
+# author: zachary.lowell@ascendengineer.com
+
+
+set -e
+
+cd colcon_ws
+
+AVAILABLE_PLATFORMS="qrb5165"
+
+print_usage(){
+    echo ""
+    echo " Build the current project based on platform target."
+    echo ""
+    echo " Usage:"
+    echo ""
+    echo "  ./build.sh qrb5165"
+    echo "        Build 64-bit binaries for qrb5165"
+    echo ""
+    echo ""
+}
+
+
+
+case "$1" in
+    qrb5165)
+        ROS_DIST="foxy"
+        ;;
+
+    *)
+        print_usage
+        exit 1
+        ;;
+esac
+
+. /opt/ros/${ROS_DIST}/setup.bash
+
+colcon build
+
+cd ..
+
+mkdir -p misc_files/opt/ros/${ROS_DIST}/
+cp -r colcon_ws/install/lib/  misc_files/opt/ros/${ROS_DIST}/
+cp -r colcon_ws/install/share/  misc_files/opt/ros/${ROS_DIST}/
+chmod -R 777 misc_files
---
+++ clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Modal AI Inc. 2023
+# author: zachary.lowell@ascendengineer.com
+
+
+rm -rf build/
+rm -rf build32/
+rm -rf build64/
+rm -rf pkg/control.tar.gz
+rm -rf pkg/data/
+rm -rf pkg/data.tar.gz
+rm -rf pkg/DEB/
+rm -rf pkg/IPK/
+rm -rf *.deb
+rm -rf .bash_history
+
+rm -rf colcon_ws/build/
+rm -rf colcon_ws/install/
+rm -rf colcon_ws/devel/
+rm -rf misc_files
