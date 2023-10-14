1440ebf14b92a5fc1aad029c581d8135ef3d8d16
James <james@modalai.com>
2023-10-13 19:17:51 -0700

    remove the "echo" to bashrc from postinst in favor of proper script in profile.d

---
+++ bash_profile/source_foxy_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# if foxy is installed, load the mpa_to_ros2 setup script which will
+# also source the Foxy setup script
+
+FOXYFILE="/opt/ros/foxy/local_setup.bash"
+MPA_TO_ROS2_FILE="/opt/ros/foxy/mpa_to_ros2/install/setup.bash"
+
+if [ -f $FOXYFILE ]; then
+	source $MPA_TO_ROS2_FILE
+fi
